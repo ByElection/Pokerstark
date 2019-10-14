652bdae153c5bc5c65ef6c48d7029fe3bfbb78dc
ByElection <gonza.zarzabal@gmail.com>
2019-10-13 22:09:57 -0300

    actualizado .gitignore y agregada base de datos

---
+++ DB/pokerstark.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-10-2019 a las 03:07:14
+-- Versión del servidor: 10.3.15-MariaDB
+-- Versión de PHP: 7.3.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pokerstark`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `amigos`
+--
+
+CREATE TABLE `amigos` (
+  `id_usuario1` int(11) NOT NULL,
+  `id_usuario2` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `items`
+--
+
+CREATE TABLE `items` (
+  `id_item` int(11) NOT NULL,
+  `nombre` text COLLATE latin1_spanish_ci NOT NULL,
+  `valor` int(11) NOT NULL,
+  `rondas` int(11) NOT NULL DEFAULT 5
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugadores`
+--
+
+CREATE TABLE `jugadores` (
+  `id_usuario` int(11) NOT NULL,
+  `fichas_mesa` int(11) NOT NULL,
+  `id_item` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mesas`
+--
+
+CREATE TABLE `mesas` (
+  `id_mesa` int(11) NOT NULL,
+  `ciega_chica` int(11) NOT NULL DEFAULT 50,
+  `ciega_grande` int(11) NOT NULL DEFAULT 100,
+  `pozo` int(11) NOT NULL DEFAULT 0,
+  `silla1` int(11) DEFAULT NULL,
+  `silla2` int(11) DEFAULT NULL,
+  `silla3` int(11) DEFAULT NULL,
+  `silla4` int(11) DEFAULT NULL,
+  `silla5` int(11) DEFAULT NULL,
+  `silla6` int(11) DEFAULT NULL,
+  `silla7` int(11) DEFAULT NULL,
+  `silla8` int(11) DEFAULT NULL,
+  `silla9` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `username` text COLLATE latin1_spanish_ci NOT NULL,
+  `password` text COLLATE latin1_spanish_ci NOT NULL,
+  `nombre` text COLLATE latin1_spanish_ci NOT NULL,
+  `apellido` text COLLATE latin1_spanish_ci NOT NULL,
+  `pais` text COLLATE latin1_spanish_ci NOT NULL,
+  `fichas` int(11) NOT NULL DEFAULT 2000
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `amigos`
+--
+ALTER TABLE `amigos`
+  ADD PRIMARY KEY (`id_usuario1`,`id_usuario2`),
+  ADD KEY `id_usuario2` (`id_usuario2`);
+
+--
+-- Indices de la tabla `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id_item`);
+
+--
+-- Indices de la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `id_item` (`id_item`);
+
+--
+-- Indices de la tabla `mesas`
+--
+ALTER TABLE `mesas`
+  ADD PRIMARY KEY (`id_mesa`),
+  ADD UNIQUE KEY `silla1` (`silla1`),
+  ADD UNIQUE KEY `silla2` (`silla2`),
+  ADD UNIQUE KEY `silla3` (`silla3`),
+  ADD UNIQUE KEY `silla4` (`silla4`),
+  ADD UNIQUE KEY `silla5` (`silla5`),
+  ADD UNIQUE KEY `silla6` (`silla6`),
+  ADD UNIQUE KEY `silla7` (`silla7`),
+  ADD UNIQUE KEY `silla8` (`silla8`),
+  ADD UNIQUE KEY `silla9` (`silla9`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `items`
+--
+ALTER TABLE `items`
+  MODIFY `id_item` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `mesas`
+--
+ALTER TABLE `mesas`
+  MODIFY `id_mesa` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `amigos`
+--
+ALTER TABLE `amigos`
+  ADD CONSTRAINT `amigos_ibfk_1` FOREIGN KEY (`id_usuario1`) REFERENCES `usuarios` (`id_usuario`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `amigos_ibfk_2` FOREIGN KEY (`id_usuario2`) REFERENCES `usuarios` (`id_usuario`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  ADD CONSTRAINT `jugadores_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `jugadores_ibfk_2` FOREIGN KEY (`id_item`) REFERENCES `items` (`id_item`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mesas`
+--
+ALTER TABLE `mesas`
+  ADD CONSTRAINT `mesas_ibfk_1` FOREIGN KEY (`silla1`) REFERENCES `jugadores` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `mesas_ibfk_2` FOREIGN KEY (`silla2`) REFERENCES `jugadores` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `mesas_ibfk_3` FOREIGN KEY (`silla3`) REFERENCES `jugadores` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `mesas_ibfk_4` FOREIGN KEY (`silla4`) REFERENCES `jugadores` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `mesas_ibfk_5` FOREIGN KEY (`silla5`) REFERENCES `jugadores` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `mesas_ibfk_6` FOREIGN KEY (`silla6`) REFERENCES `jugadores` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `mesas_ibfk_7` FOREIGN KEY (`silla7`) REFERENCES `jugadores` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `mesas_ibfk_8` FOREIGN KEY (`silla8`) REFERENCES `jugadores` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `mesas_ibfk_9` FOREIGN KEY (`silla9`) REFERENCES `jugadores` (`id_usuario`) ON DELETE SET NULL ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
