dada341d26b18b9c80af21ed217be32bc9bb5aaa
ByElection <gonza.zarzabal@gmail.com>
2019-10-21 15:08:16 -0300

    acomodado password_hash

--- Models/RegisterModel.php
+++ Models/RegisterModel.php
@@ -11,21 +11,20 @@ class RegisterModel{
     $usuarios = $this->db->prepare("SELECT username FROM usuarios WHERE username=?");
     $ok=$usuarios->execute(array($user));
     if(!$ok){
-             var_dump($usuarios->errorInfo());
-            die();
-        }
-
+      var_dump($usuarios->errorInfo());
+      die();
+    }
     $username = $usuarios->fetch(PDO::FETCH_OBJ);
     if (isset($username)&&$username['username'] === $user) { // if user exists
-        echo "el usuario ya existe";
+      echo "el usuario ya existe";
     }else{
-      $password =  password_hash($password);
+      $password =  password_hash($password, PASSWORD_DEFAULT);
       $usuarios = $this->db->prepare("INSERT INTO usuarios (username, password, nombre, apellido, pais) VALUES (?,?,?,?,?)");
       $ok=$usuarios->execute(array($user,$password,$nombre,$apellido,$pais));
       if(!$ok){
-             var_dump($usuarios->errorInfo());
-            die();
-        }
+        var_dump($usuarios->errorInfo());
+        die();
+      }
 
     }
   }
