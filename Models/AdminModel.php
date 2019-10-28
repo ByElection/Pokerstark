b0fe07ab18b704e774da20e8b1710752d6b9a06d
juangrela96 <grela_96@hotmail.com>
2019-10-27 21:26:49 -0300

    agregado funciones ABC, falta en controller

--- Models/AdminModel.php
+++ Models/AdminModel.php
@@ -7,4 +7,18 @@
         $this->db = new PDO('mysql:host=localhost;'.'dbname=pokerstark;charset=utf8', 'root', '');
     }
 
-    public function addMesa($)
+    public function addMesa($ciegas,$pozo,$sillas) {
+        $sentencia = $this->db->prepare("INSERT INTO mesa(id_ciegas,pozo, sillas) VALUES(?,?,?)");
+        $sentencia->execute(array($ciegas,$pozo,$sillas));
+    }
+
+    public function editMesa($id,$ciegas,$pozo,$sillas) {
+        $sentencia =  $this->db->prepare("UPDATE mesa SET ciegas=?, pozo=?, sillas=? WHERE id=?");
+        $sentencia->execute(array($ciegas, $pozo, $sillas, $id));
+    }
+
+    public function deletMesa($id) {
+        $sentencia = $this->db->prepare("DELETE FROM mesa WHERE id=?");
+        $sentencia->execute(array($id));
+    }
+}
