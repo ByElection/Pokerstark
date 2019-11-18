d6cf8a91998a070ec9ab4d9fc4db00446f0aea80
ByElection <gonza.zarzabal@gmail.com>
2019-11-18 19:00:47 -0300

    agregados roots de api

--- api/route-api.php
+++ api/route-api.php
@@ -1,6 +1,6 @@
 <?php
     require_once('Router.php');
-    require_once('./api/task.api.controller.php');
+    require_once('./api/AvatarsApiController.php');
 
     // CONSTANTES PARA RUTEO
     define("BASE_URL", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
@@ -9,7 +9,9 @@
 
     // rutas
     
-
+    $r->addRoute("addavatar", "POST", "AvatarsApiController", "addAvatar");
+    $r->addRoute("getavatars", "POST", "AvatarsApiController", "getAvatars");
+    
 
     //run
     $router->route($_GET['resource'], $_SERVER['REQUEST_METHOD']);
