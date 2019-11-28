ad29066dc41215789dba2a1bdb47933d6ab40844
juangrela96 <grela_96@hotmail.com>
2019-11-28 19:31:46 -0300

    arreglado los checkadmin

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -3,69 +3,59 @@ include_once('./Views/AdminView.php');
 include_once('./Models/JugadoresModel.php');
 include_once('./Models/MesasModel.php');
 include_once('./Models/CiegasModel.php');
+include_once('LoginController.php');
 
 class AdminController {
+  private $check;
   private $view;
   private $modeljugadores;
   private $modelmesas;
   private $modelciegas;
 
   public function __construct() {
+    $this->check = new LoginController();
     $this->view = new AdminView();
     $this->modeljugadores = new JugadoresModel();
     $this->modelmesas = new MesasModel();
     $this->modelciegas = new CiegasModel();
   }
 
-  public function checkLogIn(){
-          session_start();
-          if(!isset($_SESSION['id_usuario']) || ($_SESSION['admin'])==0){
-              header("Location: " . LOGIN);
-              session_destroy();
-          }
-
-          if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 5000)) {
-              header("Location: " . LOGIN);
-              session_destroy();
-          }
-          $_SESSION['LAST_ACTIVITY'] = time();
-      }
   public function mesasAdmin() {
-    $this->checkLogIn();
-    $admin=$this->checkAdmin();
+    $this->check->checkLogIn();
+    $admin=$this->check->checkAdmin();
     $mesas = $this->modelmesas->getMesas();
     $ciegas = $this->modelciegas->getCiegas();
     $jugadores = $this->modeljugadores->getJugadores();
     $this->view->mesasAdmin($admin,$mesas,$ciegas,$jugadores);
   }
   public function ciegasAdmin() {
-    $this->checkLogIn();
-    $admin=$this->checkAdmin();
+    $this->check->checkLogIn();
+    $this->check->checkAdmin();
     $ciegas = $this->modelciegas->getCiegas();
-    $this->view->ciegasAdmin($admin,$ciegas);
+    $this->view->ciegasAdmin($this->check->checkAdmin(),$ciegas);
   }
   public function addMesa() {
-    $this->checkLogIn();
+    $this->check->checkLogIn();
     $ciegas = $_POST['ciegas'];
     $sillas = $_POST['sillas'];
     $this->modelmesas->addMesa($ciegas,$sillas);
     header("Location: " . ADMIN);
   }
   public function addCiega() {
-    $this->checkLogIn();
+    $this->check->checkLogIn();
     $ciega_chica = $_POST['ciega_chica'];
     $ciega_grande = $ciega_chica * 2;
     $this->modelciegas->addCiega($ciega_chica,$ciega_grande);
     header("Location: " . CIEGASADMIN);
   }
   public function deletMesa($id) {
-    $this->checkLogIn();
+    $this->check->checkLogIn();
     $this->modeljugadores->deletJugadoresByMesa($id[":ID"]);
     $this->modelmesas->deletMesaByMesa($id[":ID"]);
     header("Location: " . ADMIN);
   }
   public function deletCiega($id) {
-    $this->checkLogIn();
+    $this->check->checkLogIn();
     $mesas=$this->modelmesas->getMesasByCiega($id[":ID"]);
     foreach ($mesas as $mesa) {
       $this->modeljugadores->deletJugadoresByMesa($mesa->id_mesa);
@@ -75,22 +65,22 @@ class AdminController {
     header("Location: " . CIEGASADMIN);
   }
   public function editMesa($id) {
-    $this->checkLogIn();
+    $this->check->checkLogIn();
     $ciegas = $_POST['ciegas'];
     $sillas = $_POST['sillas'];
     $this->modelmesas->editMesa($id[":ID"],$ciegas,$sillas);
     header("Location: " . ADMIN);
   }
   public function editCiega($id) {
-    $this->checkLogIn();
+    $this->check->checkLogIn();
     $ciega_chica = $_POST['ciega_chica'];
     $ciega_grande = strval($ciega_chica * 2);
     $this->modelciegas->editCiega($id[":ID"],$ciega_chica,$ciega_grande);
     header("Location: " . CIEGASADMIN);
   }
   public function getMesa($id) {
-    $this->checkLogIn();
-    $admin=$this->checkAdmin();
+    $this->check->checkLogIn();
+    $admin=$this->check->checkAdmin();
     $mesa = $this->modelmesas->getMesa($id[":ID"]);
     $mesas = $this->modelmesas->getMesas();
     $ciegas = $this->modelciegas->getCiegas();
@@ -98,21 +88,11 @@ class AdminController {
     $this->view->mesasAdmin($admin,$mesas,$ciegas,$jugadores,$mesa);
   }
   public function getCiega($id) {
-    $this->checkLogIn();
-    $admin=$this->checkAdmin();
+    $this->check->checkLogIn();
+    $admin=$this->check->checkAdmin();
     $ciega = $this->modelciegas->getCiegasById($id[":ID"]);
     $ciegas = $this->modelciegas->getCiegas();
     $this->view->ciegasAdmin($admin,$ciegas,$ciega);
-  }
-  public function checkAdmin() {
-    if (isset($_SESSION['admin'])){
-      if ($_SESSION['admin']!=0) {
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
   }
 }
   ?>
