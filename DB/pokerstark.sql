b1466d83593de30004e0be5245091a82c0a13fb4
ByElection <gonza.zarzabal@gmail.com>
2021-08-11 21:12:38 -0300

    actualizacion de DB

--- DB/pokerstark.sql
+++ DB/pokerstark.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-11-2019 a las 21:27:49
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.9
+-- Tiempo de generación: 12-08-2021 a las 02:10:18
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `pokerstark`
 --
-CREATE DATABASE IF NOT EXISTS `pokerstark` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `pokerstark`;
 
 -- --------------------------------------------------------
 
@@ -48,6 +45,30 @@ INSERT INTO `avatars` (`id_avatar`, `img`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `chat`
+--
+
+CREATE TABLE `chat` (
+  `id_mesa` int(11) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `mensaje` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `chat`
+--
+
+INSERT INTO `chat` (`id_mesa`, `id_usuario`, `mensaje`) VALUES
+(14, 1, 'hola'),
+(14, 1, 'agarrame los huevo'),
+(14, 1, 'agarrame los huevo'),
+(14, 1, 'agarrame los huevo'),
+(14, 1, 'agarrame los huevo'),
+(14, 1, 'agarrame los huevo');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `ciegas`
 --
 
@@ -62,8 +83,8 @@ CREATE TABLE `ciegas` (
 --
 
 INSERT INTO `ciegas` (`id_ciegas`, `ciega_chica`, `ciega_grande`) VALUES
-(13, 4545, 9090),
-(15, 55, 110),
+(13, 10, 20),
+(15, 50, 100),
 (16, 200, 400),
 (17, 100, 200);
 
@@ -76,7 +97,11 @@ INSERT INTO `ciegas` (`id_ciegas`, `ciega_chica`, `ciega_grande`) VALUES
 CREATE TABLE `jugadores` (
   `id_usuario` int(11) NOT NULL,
   `fichas_mesa` int(11) NOT NULL,
-  `id_mesa` int(11) NOT NULL
+  `id_mesa` int(11) NOT NULL,
+  `silla` int(11) NOT NULL,
+  `cartas` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `apuesta` int(11) NOT NULL DEFAULT 0,
+  `retirado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 -- --------------------------------------------------------
@@ -89,16 +114,39 @@ CREATE TABLE `mesas` (
   `id_mesa` int(11) NOT NULL,
   `id_ciegas` int(11) NOT NULL,
   `pozo` int(11) NOT NULL DEFAULT 0,
-  `sillas` int(11) NOT NULL DEFAULT 9
+  `sillas` int(11) NOT NULL DEFAULT 9,
+  `mazo` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`mazo`)),
+  `dealer` int(11) DEFAULT 1,
+  `turno` int(11) DEFAULT 1,
+  `fase` int(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `mesas`
 --
 
-INSERT INTO `mesas` (`id_mesa`, `id_ciegas`, `pozo`, `sillas`) VALUES
-(10, 16, 0, 3),
-(11, 16, 0, 9);
+INSERT INTO `mesas` (`id_mesa`, `id_ciegas`, `pozo`, `sillas`, `mazo`, `dealer`, `turno`, `fase`) VALUES
+(14, 13, 0, 5, '[{\"palo\":\"C\",\"valor\":\"8\"},{\"palo\":\"C\",\"valor\":\"3\"},{\"palo\":\"P\",\"valor\":\"12\"},{\"palo\":\"P\",\"valor\":\"8\"},{\"palo\":\"C\",\"valor\":\"6\"},{\"palo\":\"P\",\"valor\":\"6\"},{\"palo\":\"C\",\"valor\":\"13\"},{\"palo\":\"D\",\"valor\":\"9\"},{\"palo\":\"P\",\"valor\":\"11\"},{\"palo\":\"P\",\"valor\":\"9\"},{\"palo\":\"C\",\"valor\":\"2\"},{\"palo\":\"T\",\"valor\":\"8\"},{\"palo\":\"C\",\"valor\":\"7\"},{\"palo\":\"D\",\"valor\":\"5\"},{\"palo\":\"P\",\"valor\":\"3\"},{\"palo\":\"P\",\"valor\":\"1\"},{\"palo\":\"T\",\"valor\":\"13\"},{\"palo\":\"D\",\"valor\":\"2\"},{\"palo\":\"C\",\"valor\":\"4\"},{\"palo\":\"T\",\"valor\":\"4\"},{\"palo\":\"P\",\"valor\":\"13\"},{\"palo\":\"P\",\"valor\":\"5\"},{\"palo\":\"D\",\"valor\":\"11\"},{\"palo\":\"P\",\"valor\":\"10\"},{\"palo\":\"T\",\"valor\":\"9\"},{\"palo\":\"T\",\"valor\":\"6\"},{\"palo\":\"C\",\"valor\":\"11\"},{\"palo\":\"D\",\"valor\":\"3\"},{\"palo\":\"P\",\"valor\":\"2\"},{\"palo\":\"C\",\"valor\":\"1\"},{\"palo\":\"D\",\"valor\":\"12\"},{\"palo\":\"T\",\"valor\":\"11\"},{\"palo\":\"T\",\"valor\":\"1\"},{\"palo\":\"C\",\"valor\":\"5\"},{\"palo\":\"T\",\"valor\":\"3\"},{\"palo\":\"D\",\"valor\":\"1\"},{\"palo\":\"D\",\"valor\":\"10\"},{\"palo\":\"D\",\"valor\":\"7\"},{\"palo\":\"P\",\"valor\":\"4\"},{\"palo\":\"D\",\"valor\":\"13\"},{\"palo\":\"P\",\"valor\":\"7\"},{\"palo\":\"T\",\"valor\":\"12\"},{\"palo\":\"C\",\"valor\":\"10\"},{\"palo\":\"C\",\"valor\":\"12\"},{\"palo\":\"T\",\"valor\":\"5\"},{\"palo\":\"T\",\"valor\":\"10\"},{\"palo\":\"D\",\"valor\":\"4\"},{\"palo\":\"D\",\"valor\":\"6\"},{\"palo\":\"C\",\"valor\":\"9\"},{\"palo\":\"T\",\"valor\":\"7\"},{\"palo\":\"T\",\"valor\":\"2\"},{\"palo\":\"D\",\"valor\":\"8\"}]', 1, 1, 0),
+(15, 13, 0, 6, '[{\"palo\":\"D\",\"valor\":\"9\"},{\"palo\":\"P\",\"valor\":\"4\"},{\"palo\":\"P\",\"valor\":\"5\"},{\"palo\":\"C\",\"valor\":\"1\"},{\"palo\":\"P\",\"valor\":\"9\"},{\"palo\":\"C\",\"valor\":\"13\"},{\"palo\":\"T\",\"valor\":\"7\"},{\"palo\":\"C\",\"valor\":\"9\"},{\"palo\":\"P\",\"valor\":\"8\"},{\"palo\":\"P\",\"valor\":\"3\"},{\"palo\":\"P\",\"valor\":\"11\"},{\"palo\":\"T\",\"valor\":\"2\"},{\"palo\":\"T\",\"valor\":\"9\"},{\"palo\":\"D\",\"valor\":\"7\"},{\"palo\":\"D\",\"valor\":\"10\"},{\"palo\":\"D\",\"valor\":\"13\"},{\"palo\":\"D\",\"valor\":\"5\"},{\"palo\":\"P\",\"valor\":\"2\"},{\"palo\":\"C\",\"valor\":\"12\"},{\"palo\":\"C\",\"valor\":\"2\"},{\"palo\":\"T\",\"valor\":\"6\"},{\"palo\":\"T\",\"valor\":\"4\"},{\"palo\":\"D\",\"valor\":\"6\"},{\"palo\":\"P\",\"valor\":\"6\"},{\"palo\":\"C\",\"valor\":\"3\"},{\"palo\":\"C\",\"valor\":\"7\"},{\"palo\":\"D\",\"valor\":\"3\"},{\"palo\":\"T\",\"valor\":\"5\"},{\"palo\":\"C\",\"valor\":\"5\"},{\"palo\":\"P\",\"valor\":\"13\"},{\"palo\":\"D\",\"valor\":\"2\"},{\"palo\":\"P\",\"valor\":\"7\"},{\"palo\":\"C\",\"valor\":\"10\"},{\"palo\":\"P\",\"valor\":\"12\"},{\"palo\":\"D\",\"valor\":\"1\"},{\"palo\":\"D\",\"valor\":\"8\"},{\"palo\":\"D\",\"valor\":\"11\"},{\"palo\":\"T\",\"valor\":\"11\"},{\"palo\":\"T\",\"valor\":\"10\"},{\"palo\":\"C\",\"valor\":\"6\"},{\"palo\":\"T\",\"valor\":\"12\"},{\"palo\":\"C\",\"valor\":\"4\"},{\"palo\":\"T\",\"valor\":\"1\"},{\"palo\":\"P\",\"valor\":\"10\"},{\"palo\":\"C\",\"valor\":\"8\"},{\"palo\":\"T\",\"valor\":\"8\"},{\"palo\":\"D\",\"valor\":\"12\"},{\"palo\":\"C\",\"valor\":\"11\"},{\"palo\":\"T\",\"valor\":\"3\"},{\"palo\":\"T\",\"valor\":\"13\"},{\"palo\":\"P\",\"valor\":\"1\"},{\"palo\":\"D\",\"valor\":\"4\"}]', 1, 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `puntajes`
+--
+
+CREATE TABLE `puntajes` (
+  `id_usuario` int(11) NOT NULL,
+  `id_mesa` int(11) NOT NULL,
+  `puntaje` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `puntajes`
+--
+
+INSERT INTO `puntajes` (`id_usuario`, `id_mesa`, `puntaje`) VALUES
+(1, 14, 5);
 
 -- --------------------------------------------------------
 
@@ -123,7 +171,7 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `username`, `password`, `nombre`, `apellido`, `pais`, `fichas`, `id_avatar`, `admin`) VALUES
-(1, 'ByElection', '$2y$10$3ZZ.tShVVlrBwJw/iWwuBezFfj2r2KAUhhZSjNhsJqz4UENbFE6OC', 'Gonzalo', 'Zarzabal', 'Argentina', 207000, 1, 1),
+(1, 'ByElection', '$2y$10$3ZZ.tShVVlrBwJw/iWwuBezFfj2r2KAUhhZSjNhsJqz4UENbFE6OC', 'Gonzalo', 'Zarzabal', 'Argentina', 209000, 1, 1),
 (2, 'robertito', '$2y$10$udMc/EsZsv4yPYMxYDbGruewerBP2tnzAn38HDqNuLroMDdhidGSa', 'roberto', 'carlos', 'Brasil', 30000, 1, 0),
 (3, 'pechofrio', '$2y$10$ICEY9uvast08K7SOs3G15OsZzuS7T3QXuYsswYiOkx2dXCSG6lQPq', 'Lionel', 'Messi', 'España', 10000, 1, 0),
 (4, 'EbolaKills', '$2y$10$jctRdE5GVKbcWAgg.8UzruWPntZNV3SmSgnzSBpmjh2ULJeiU7zhG', 'Juan', 'Grela', 'Bosnia y Herzegovina', 2000, 1, 1);
@@ -160,6 +208,13 @@ ALTER TABLE `mesas`
   ADD KEY `id_ciegas` (`id_ciegas`);
 
 --
+-- Indices de la tabla `puntajes`
+--
+ALTER TABLE `puntajes`
+  ADD PRIMARY KEY (`id_usuario`,`id_mesa`),
+  ADD KEY `id_mesa` (`id_mesa`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -187,7 +242,7 @@ ALTER TABLE `ciegas`
 -- AUTO_INCREMENT de la tabla `mesas`
 --
 ALTER TABLE `mesas`
-  MODIFY `id_mesa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_mesa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -211,6 +266,13 @@ ALTER TABLE `jugadores`
 --
 ALTER TABLE `mesas`
   ADD CONSTRAINT `mesas_ibfk_1` FOREIGN KEY (`id_ciegas`) REFERENCES `ciegas` (`id_ciegas`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `puntajes`
+--
+ALTER TABLE `puntajes`
+  ADD CONSTRAINT `puntajes_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `puntajes_ibfk_2` FOREIGN KEY (`id_mesa`) REFERENCES `mesas` (`id_mesa`);
 
 --
 -- Filtros para la tabla `usuarios`
